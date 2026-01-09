0d20027b4b1c8f3cabf85cbad28eb027f5389b08
Tafadzwa Emmanuel Njagu <mcnashten@gmail.com>
2026-01-09 21:59:45 +0200

    Add deployment files

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install python dependencies
+pip install -r backend/requirements.txt
+
+# Collect static files
+python backend/manage.py collectstatic --no-input
+
+# Run migrations
+python backend/manage.py migrate
